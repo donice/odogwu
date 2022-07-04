aedeb2a5a79f79ac75cd59b096531a5c7afd26b8
Donice Ubaru <doniceubaru1@gmail.com>
2022-07-04 03:05:51 +0100

    odogwu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className= 'gradient-bg-welcome'>
+    <div className="inner-card blue-glassmorphism">
+      <h1 className="header">Odogwu</h1>
+      <div className="para">
+        <p>Hello Lad, I won</p>
+      </div>
     </div>
+  </div>  
+  
   );
-}
+};
 
 export default App;
